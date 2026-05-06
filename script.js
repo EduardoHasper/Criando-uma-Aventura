59c6e9b04e8915169c1f49aa6e3fc87d594385a0
EduardoHasper <eduardo.hasper.pereira@escola.pr.gov.br>
2026-05-06 09:41:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+<div style="padding: 20px; background-color: #f0f0f0; text-align: center;">
+    <p>Nesta sala parece não haver nada além de vento...</p>
+    
+    <button style="opacity: 0; cursor: pointer; border: none; background: none;" onclick="alert('Você encontrou a Rota Alternativa!')">
+        [ BOTÃO SECRETO ]
+    </button>
+</div>
